236e9171f11cf44586fa8b1d0f312cf16fb8aa07
Måns Oskarsson <manso@DESKTOP-K42UUPG.localdomain>
2023-11-30 11:58:37 +0100

    changes to readme and added house

---
+++ code/application-energy-house/src/main/java/eu/arrowhead/application/provider/service/ElectricityGenerationConstants.java
@@ -0,0 +1,14 @@
+package eu.arrowhead.application.provider.service;
+
+public class ElectricityGenerationConstants {
+
+    static final int N_SOLARPANELS = 12;
+
+    static final float ELECTRICITY = 2;
+
+    static final String TYPE = "kW/h";
+
+    static final float MIN_EFFICIENCY = 0.7f;
+    static final float MAX_EFFICIENCY = 1.2f;
+    
+}
